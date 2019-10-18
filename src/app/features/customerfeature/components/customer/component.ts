b931265822aa1739b160924c1612312d3d58b91d
pokalwas <Satish.Pokalwar@mitel.com>
2019-10-18 12:58:51 +0530

    added cusotmer module again

---
+++ src/app/features/customerfeature/components/customer/component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-customer',
+    templateUrl:'./component.html',
+    styleUrls:['./component.scss']
+})
+export class CustomerComponent{
+
+}
