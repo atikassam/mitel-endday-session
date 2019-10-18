2800ca256e8d84e989493611925dfc5598cf6c0d
karuncm <karuncm@gmail.com>
2019-10-18 14:08:50 +0530

    Add-item page added

--- src/app/features/inventory/pages/add-item/add-item.component.ts
+++ src/app/features/inventory/pages/add-item/add-item.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-item',
+  //selector: 'app-add-item',
   templateUrl: './add-item.component.html',
   styleUrls: ['./add-item.component.scss']
 })
 export class AddItemComponent implements OnInit {
-
+  inventory:any = {}
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddSubmit(){
+    console.log(this.inventory);
   }
 
 }
