8f0e3f54ad44682573ba43d37f2942214c3872ee
MITEL\kumarsar <saravana.kumar@mitel.com>
2019-10-18 12:17:57 +0530

    Sales Module created

---
+++ src/app/features/salesModule/components/component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+
+
+@Component({
+    selector: 'app-sales',
+    templateUrl: './component.html'
+})
+
+export class SalesComponent {
+
+}
---
+++ src/app/features/salesModule/sales.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SalesComponent } from './components/component';
+
+@NgModule({
+    declarations: [
+        SalesComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: []
+})
+export class SalesModule { }
