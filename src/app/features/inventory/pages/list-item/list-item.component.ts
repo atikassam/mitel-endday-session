871b2e050d69ecf4ac91464405655efd6166827a
Pitabas Pradhan <Pitabas.Pradhan@mitel.com>
2019-10-18 12:37:38 +0530

    added pages for inventory

---
+++ src/app/features/inventory/pages/list-item/list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss']
+})
+export class ListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
